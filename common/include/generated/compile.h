5c9922053456dd5aada636db46cfef2539f0fcdf
Lopicl <lopicl.00@gmail.com>
2013-08-25 13:23:46 +0200

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Sat Aug 24 21:29:39 CEST 2013"
+#define LINUX_COMPILE_TIME "21:29:39"
+#define LINUX_COMPILE_BY "matteo"
+#define LINUX_COMPILE_HOST "HP-Pavilion-dv6"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
